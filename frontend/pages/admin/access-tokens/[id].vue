6e976cb355c6d50774cb3584fd0c86a6e1b93ad7
Chloé <chloe.g14@outlook.fr>
2025-11-01 22:25:53 +0100

    Modif bouton générer
    Modification du bouton de génération d'UUID pour qu'il s'intègre dans le thème SafeHaven

--- frontend/pages/admin/access-tokens/[id].vue
+++ frontend/pages/admin/access-tokens/[id].vue
@@ -21,16 +21,17 @@
       />
 
       <Button
+        outlined
+        rounded
         type="button"
-        class="whitespace-nowrap"
+        class="mx-2"
         aria-label="générer un UUID"
         style="align-self: flex-start; margin-top: 2rem;"
         @click="generateUUID"
       >
-        <AppIcon
-          icon-name="dice"
-          class="w-4 h-4"
-        />
+      <template #icon>
+        <AppIcon icon-name="dice" />
+      </template>
       </Button>
     </div>
 
